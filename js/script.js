00d68500f66a0de5db633e7813cf20a32d651424
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-03-14 14:17:05 +0100

    feat: complete milestone 3

---
+++ js/script.js
@@ -0,0 +1,37 @@
+const gridElement = document.getElementById('grid');
+
+const endpoint = 'https://lanciweb.github.io/demo/api/pictures/';
+
+axios.get(endpoint)
+    .then(response => {
+        const data = response.data;
+        renderHTML(gridElement, data);
+    });
+
+
+function renderHTML(parent, elements) {
+    let item = '';
+
+
+    for (let i = 0; i < elements.length; i++) {
+        let currentPhoto = elements[i];
+        item += makeStructureCardHTML(currentPhoto);
+    }
+    parent.innerHTML = item;
+}
+
+function makeStructureCardHTML(photo) {
+    return `<div class="card">
+                <div class="card-top">
+                    <img src="img/pin.svg" alt="Pin" class="img-pin" />
+                    <img
+                        src=${photo.url}
+                        alt=${photo.title} />
+                </div>
+                <div class="card-bottom">
+                    <p class="card-info">
+                    ${photo.title}
+                    </p>
+                </div>
+            </div>`;
+}
